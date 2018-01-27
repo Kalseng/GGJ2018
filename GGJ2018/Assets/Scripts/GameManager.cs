39552d405f8d8c8e3df8c157b5af85627389ac32
Marcus Holm <mqholm@gmail.com>
2018-01-27 15:08:30 -0500

    Make An Actual Game
    Added timer, other stuff

--- GGJ2018/Assets/Scripts/GameManager.cs
+++ GGJ2018/Assets/Scripts/GameManager.cs
@@ -1,58 +1,110 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using XInputDotNetPure;
 
 public class GameManager : MonoBehaviour {
 
+    // Publicly assigned variables for game control.
     public Ghost g1;
     public AntennaRotator ar;
 
-    private bool interactableTV = true;
+    // For managing interaction on/off state.
+    private bool interactableTV = false;
+
+    // Variables for storing ghost locations. Consider moving to Ghost script.
+    public float leftDiff, rightDiff;
 
     private Quaternion ghostLeftQuat, ghostRightQuat;
     private Quaternion antennaLeftQuat, antennaRightQuat;
-    public float leftDiff, rightDiff;
+
+    // Variables for round timer
+    public Image clock;
+    public float roundLength = 30.0f;
+
+    private float roundStartTime;
+
+    // Lists of ghosts for rounds, denomonated by last number.
+    private List<List<Ghost>> ghostRounds;
+    public List<Ghost> ghostList1, ghostList2, ghostList3, ghostList4;
+
+    // Round index
+    public int roundIndex;
 
 	// Use this for initialization
 	void Start () {
-
-        g1.rightFrequency += ar.rightAntennaRotationStart;
-        g1.leftFrequency += ar.leftAntennaRotationStart;
+        ghostRounds.Add(ghostList1);
+        ghostRounds.Add(ghostList2);
+        ghostRounds.Add(ghostList3);
+        ghostRounds.Add(ghostList4);
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+        if (Input.GetButtonDown("Start"))
+        {
+            FreshStart();
+        }
+
         if (interactableTV)
         {
-            {
-                ar.ProessRotation();
-
-                ghostLeftQuat = Quaternion.Euler(g1.leftFrequency);
-                ghostRightQuat = Quaternion.Euler(g1.rightFrequency);
-
-                antennaLeftQuat = Quaternion.Euler(ar.leftAntenna.eulerAngles);
-                antennaRightQuat = Quaternion.Euler(ar.rightAntenna.eulerAngles);
-
-                leftDiff = Quaternion.Angle(ghostLeftQuat, antennaLeftQuat);
-                rightDiff = Quaternion.Angle(ghostRightQuat, antennaRightQuat);
-
-                leftDiff = 180.0f - leftDiff;
-                rightDiff = 180.0f - rightDiff;
-
-                if (leftDiff < 5.0f || rightDiff < 5.0f )
-                {
-                    GamePad.SetVibration(0, 1.0f - leftDiff / 5.0f, 1.0f - rightDiff / 5.0f);
-                }
-                else
-                {
-                    GamePad.SetVibration(0, 0, 0);
-                }
-            }
-            
+            ProcessAntennae();
+            ProcessTime();
+        }
+        else{
+            GamePad.SetVibration(0, 0, 0);
         }
         
 	}
+
+    void ProcessTime()
+    {
+        if(Time.time - roundStartTime >= roundLength){
+            clock.fillAmount = 0.0f;
+            interactableTV = false;
+        }
+        clock.fillAmount = 1 - (Time.time - roundStartTime) / roundLength;
+    }
+
+    void ProcessAntennae ()
+    {
+
+        ar.ProessRotation();
+
+        ghostLeftQuat = Quaternion.Euler(g1.leftFrequency);
+        ghostRightQuat = Quaternion.Euler(g1.rightFrequency);
+
+        antennaLeftQuat = Quaternion.Euler(ar.leftAntenna.eulerAngles);
+        antennaRightQuat = Quaternion.Euler(ar.rightAntenna.eulerAngles);
+
+        leftDiff = Quaternion.Angle(ghostLeftQuat, antennaLeftQuat);
+        rightDiff = Quaternion.Angle(ghostRightQuat, antennaRightQuat);
+
+        leftDiff = 180.0f - leftDiff;
+        rightDiff = 180.0f - rightDiff;
+
+        if (leftDiff < 5.0f || rightDiff < 5.0f)
+        {
+            GamePad.SetVibration(0, 1.0f - leftDiff / 5.0f, 1.0f - rightDiff / 5.0f);
+        }
+        else
+        {
+            GamePad.SetVibration(0, 0, 0);
+        }
+    }
+
+    void FreshStart()
+    {
+        interactableTV = true;
+
+        g1.RandomizeFrequency();
+
+        g1.rightFrequency += ar.rightAntennaRotationStart;
+        g1.leftFrequency += ar.leftAntennaRotationStart;
+
+        roundStartTime = Time.time;
+    }
 }
--- GGJ2018/Assets/Scripts/Ghost.cs
+++ GGJ2018/Assets/Scripts/Ghost.cs
@@ -6,20 +6,29 @@ public class Ghost : MonoBehaviour {
 
     public AudioClip broadcastAudio;
     public Texture2D ghostPicture;
+
     public Vector3 leftFrequency, rightFrequency = new Vector3(0,0,0);
+
+    public int channel;
 
 	// Use this for initialization
 	void Start () {
-        leftFrequency.x = Random.value * -30;
-        leftFrequency.y = Random.value * -30;
-
-        rightFrequency.x = Random.value * 30;
-        rightFrequency.y = Random.value * 30;
-
+        RandomizeFrequency();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void RandomizeFrequency()
+    {
+        leftFrequency.x = Random.value * -30;
+        leftFrequency.y = Random.value * -30;
+        leftFrequency.z = 0;
+
+        rightFrequency.x = Random.value * 30;
+        rightFrequency.y = Random.value * 30;
+        rightFrequency.z = 0;
+    }
 }
