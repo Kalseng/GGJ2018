fef9b9c0fae48e5e45725fb85afe843f81add1eb
Marcus Holm <mqholm@gmail.com>
2018-01-27 03:43:27 -0500

    Make a Sex Toy
    oops

---
+++ GGJ2018/Assets/Scripts/AntennaRotator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+public class AntennaRotator : MonoBehaviour {
+
+    public Transform leftAntenna, rightAntenna;
+    public Vector3 leftAntennaRotationStart, rightAntennaRotationStart;
+    public float sensitivityModifier = 30;
+
+	// Use this for initialization
+	void Start () {
+        leftAntennaRotationStart = leftAntenna.localEulerAngles;
+        rightAntennaRotationStart = rightAntenna.localEulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float lhDegree = Input.GetAxis("HorizontalLeft") * -1 * sensitivityModifier;
+        float lvDegree = Input.GetAxis("VerticalLeft") * -1 * sensitivityModifier;
+
+        float rhDegree = Input.GetAxis("HorizontalRight") * sensitivityModifier;
+        float rvDegree = Input.GetAxis("VerticalRight") * sensitivityModifier;
+
+        leftAntenna.localRotation = Quaternion.Euler(leftAntennaRotationStart + new Vector3(lhDegree, lvDegree, 0));
+        rightAntenna.localRotation = Quaternion.Euler(rightAntennaRotationStart + new Vector3(rhDegree, rvDegree, 0));
+
+        if (Input.GetButtonDown("Vibrate"))
+        {
+            GamePad.SetVibration(0, 1, 1);
+        }
+        if (Input.GetButtonUp("Vibrate"))
+        {
+            GamePad.SetVibration(0, 0, 0);
+        }
+	}
+}
